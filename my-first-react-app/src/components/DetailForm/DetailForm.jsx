f5e19086b9e207658023ca0e1a8aa8145a1be4dc
agent-52 <abhaybhadauriya52@gmail.com>
2025-02-27 23:38:46 +0530

    add whatsapp message implementation with custom message

--- my-first-react-app/src/components/DetailForm/DetailForm.jsx
+++ my-first-react-app/src/components/DetailForm/DetailForm.jsx
@@ -6,35 +6,44 @@ const DetailForm = () =>{
     const [phone, setPhone] = useState("")
     const [email, setEmail] = useState("")
     const [date, setDate] = useState("")
-    const [message, setMessage] = useState("")
+    const [domain, setDomain] = useState("")
+    const sendWhatsAppMessage = () =>{
+        let name = document.querySelector("#name").value.trim()
+        let date = document.querySelector("#date").value.trim()
+        let domain = document.querySelector("#domain").value.trim()
+        let message = `Hello, ${name} this side. I am interested in learning ${domain}. Could you please provide details on availability and fees? I would love to have a demo class on ${date} if possible. Looking forward to your response. Thank you!`;
+        let phoneNumber = "+918817716118"; // Change this to your number
+        let url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+        window.open(url, "_blank");
+    }
     return (
-        <form action="" method="post" className="pdi2 pdb2  fs1 flexC gap2">
+        <form  onSubmit={sendWhatsAppMessage} className="pdi2 pdb2  fs1 flexC gap2">
             <h1 className="f1 fs3 cBl ">Schedule a Free Trial</h1>
             <div className="flex gap1_5">
                 <div className="flexC gap0">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" value={name} placeholder="Anshul" onChange={(name) => setName(name.target.value)}/>
+                    <label htmlFor="name">Name<span className="cBl">*</span></label>
+                    <input type="text" value={name} placeholder="Anshul" onChange={(name) => setName(name.target.value)} id="name" required/>
                 </div>
                 <div className="flexC gap0">
-                    <label htmlFor="phone">Phone <span className="cBl">*</span></label>
-                    <input type="tel" value={phone} required placeholder="8817716105" onChange={(phone) => setPhone(phone.target.value)}/>
+                    <label htmlFor="phone">Phone </label>
+                    <input type="tel" value={phone} placeholder="8817716105" onChange={(phone) => setPhone(phone.target.value)} id="phoneNumber"/>
                 </div>
             </div>
             <div className="flex gap1_5">
                 <div className="flexC gap0">
-                    <label htmlFor="email">Email <span className="cBl">*</span></label>
-                    <input type="email" value={email} required placeholder="anshul123@gmail.com" onChange={(email) => setEmail(email.target.value)}/>
+                    <label htmlFor="email">Email </label>
+                    <input type="email" value={email} placeholder="anshul123@gmail.com" onChange={(email) => setEmail(email.target.value)} id="email"/>
                 </div>
                 <div className="flexC gap0">
                     <label htmlFor="date">Date <span className="cBl">*</span></label>
-                    <input type="date" value={date} onChange={(date) => setDate(date.target.value)}/>
+                    <input type="date" value={date} onChange={(date) => setDate(date.target.value)} id="date"/>
                 </div>
             </div>
             <div className="flexC gap0">
-                <label htmlFor="message">Message</label>
-                <textarea name="message" id="message" value={message} placeholder="Hi i am anshual i learned guitar before but i am unable to play without watching a tutorial i hope you can help me out!" onChange={(message) => setMessage(message.target.value)}></textarea>
+                <label htmlFor="message">Domain<span className="cBl">*</span></label>
+                <input name="domain" id="domain" value={domain} placeholder="Singing / Flute / Guitar / Drum ..." onChange={(domain) => setDomain(domain.target.value)} ></input>
             </div>
-            <button type="submit" className="w100 bgBl cW f2 fs1 brR pdi1 pdb0">Schedule</button>
+            <button type="submit" className="w100 bgBl cW f2 fs1 brR pdi1 pdb0" >Schedule</button>
         </form>
     )
 }
